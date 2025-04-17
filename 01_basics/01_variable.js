d921dda2c9a5907dc5170be2b833ed40b96c470b
ashishuieky <ashuuikey2002@gmail.com>
2025-04-17 14:54:29 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 14444
+let accountEmail = "ashish@gmail.com"
+var accountPassword = "asldfj"
+accountCity = "Bengaluru"
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity]) 
