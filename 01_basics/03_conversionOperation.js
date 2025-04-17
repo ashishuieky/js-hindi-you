df97bea4061df14565c05c94d86efee6b9c1b9fd
ashishuieky <ashuuikey2002@gmail.com>
2025-04-17 14:55:04 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+ let str1 = "hello";
+ let str2 = " ashish"
+ let str3 = str1 + str2;
+ console.log(str3)
