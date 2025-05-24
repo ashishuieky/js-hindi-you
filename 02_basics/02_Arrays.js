689c88d797198afe676754f4744b508905f9a49d
ashishuieky <ashuuikey2002@gmail.com>
2025-05-24 05:09:52 +0000

    arrays 2

---
+++ 02_basics/02_Arrays.js
@@ -0,0 +1,11 @@
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc =  ["superman","flash","batman"]
+
+marvel_heros.push(dc)
+
+console.log(marvel_heros)
+console.log(marvel_heros[3][1]);
+
+const allHeros=marval_heros.concat(dc)
+console.log(allHeros);
+
